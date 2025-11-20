5fff3e427792a5bb86fdf3908633786324cd59bf
khimiakk <mvkh2007@gmail.com>
2025-11-20 21:07:00 +0200

    add Decorator tests

--- src/test/java/ucu/apps/flowerstorereboot/DecoratorTests.java
+++ src/test/java/ucu/apps/flowerstorereboot/DecoratorTests.java
@@ -12,12 +12,13 @@ import org.junit.jupiter.api.Test;
 
 public class DecoratorTests {
 
-    @Test
+   @Test
     public void testPaperDecorator() {
         Item flower = new Flower("Rose", "Red", 10.0, true);
         Item decoratedFlower = new PaperDecorator(flower);
-        assertEquals("Flower of type Rose with color Red, available true",
-                    decoratedFlower.getDescription());
+        assertEquals(
+            "Flower of type Rose with color Red, available true in paper.",
+            decoratedFlower.getDescription());
         assertEquals(23.0, decoratedFlower.price());
     }
 
@@ -25,8 +26,9 @@ public class DecoratorTests {
     public void testRibbonDecorator() {
         Item flower = new Flower("Tulip", "Yellow", 12.0, true);
         Item decoratedFlower = new RibbonDecorator(flower);
-        assertEquals("Flower of type Tulip with color Yellow, available true",
-                    decoratedFlower.getDescription());
+        assertEquals(
+            "Flower of type Tulip with color Yellow, available true with ribbon.",
+            decoratedFlower.getDescription());
         assertEquals(52.0, decoratedFlower.price());
     }
 
@@ -34,17 +36,23 @@ public class DecoratorTests {
     public void testBasketDecorator() {
         Item flower = new Flower("Lily", "White", 15.0, true);
         Item decoratedFlower = new BasketDecorator(flower);
-        assertEquals("Flower of type Lily with color White, available true",
-                    decoratedFlower.getDescription());
+        assertEquals(
+            "Flower of type Lily with color White, available true in a basket.",
+            decoratedFlower.getDescription());
         assertEquals(19.0, decoratedFlower.price());
     }
 
     @Test
     public void testMultipleDecorators() {
         Item flower = new Flower("Orchid", "Purple", 20.0, true);
-        Item decoratedFlower = new PaperDecorator(new RibbonDecorator(new BasketDecorator(flower)));
-        assertEquals("Flower of type Orchid with color Purple, available true",
-                    decoratedFlower.getDescription());
+        Item decoratedFlower =
+            new PaperDecorator(new RibbonDecorator(new BasketDecorator(flower)));
+
+        assertEquals(
+            "Flower of type Orchid with color Purple, available true in a basket. with ribbon. in paper.",
+            decoratedFlower.getDescription());
+
         assertEquals(77.0, decoratedFlower.price());
     }
+
 }
